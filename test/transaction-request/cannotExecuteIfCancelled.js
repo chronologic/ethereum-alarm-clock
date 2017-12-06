fdbbfb23a6d21ffd7539ca8a5629ed9afcf47ecf
lsaether <Lsaether@protonmail.com>
2017-12-06 15:50:37 -0700

    fixed cannotExe....js

--- test/transaction-request/cannotExecuteIfCancelled.js
+++ test/transaction-request/cannotExecuteIfCancelled.js
@@ -10,6 +10,7 @@ const TransactionRequest = artifacts.require('./TransactionRequest.sol')
 
 /// Bring in config.web3 (v1.0.0)
 const config = require('../../config')
+const { parseRequestData, parseAbortData, wasAborted } = require('../dataHelpers.js')
 const { wait, waitUntilBlock } = require('@digix/tempo')(web3)
 const toBN = config.web3.utils.toBN 
 
@@ -27,14 +28,14 @@ contract('tests execution rejected if cancelled', async function(accounts) {
         const curBlockNum = await config.web3.eth.getBlockNumber()
         const windowStart = curBlockNum + 38
 
-        const transactionRecorder = await TransactionRecorder.new()
+        const txRecorder = await TransactionRecorder.new()
 
-        const transactionRequest = await TransactionRequest.new(
+        const txRequest = await TransactionRequest.new(
             [
                 Owner, //createdBy
                 Owner, //owner
                 accounts[1], //donationBenefactor
-                transactionRecorder.address //toAddress
+                txRecorder.address //toAddress
             ], [
                 0, //donation
                 0, //payment
@@ -49,38 +50,41 @@ contract('tests execution rejected if cancelled', async function(accounts) {
             ],
             'some-call-data-could-be-anything'
         )
+        const requestData = await parseRequestData(txRequest)         
 
-        const wasCalled = await transactionRecorder.wasCalled()
-        assert(wasCalled === false)
+        expect(await txRecorder.wasCalled())
+        .to.be.false 
 
-        const requestData = await transactionRequest.requestData()
-        const logs = requestData.logs.find(e => e.event === 'RequestData')
-        // requestData.meta.isCancelled === false
-        assert(logs.args.bools[0] === false)
-        // requestData.meta.wasCalled === false
-        assert(logs.args.bools[1] === false)
+        expect(requestData.meta.wasCalled)
+        .to.be.false 
 
-        const cancelTx = await transactionRequest.cancel({from: Owner})
+        expect(requestData.meta.isCancelled)
+        .to.be.false
+
+        const cancelTx = await txRequest.cancel({from: Owner})
         expect(cancelTx.receipt).to.exist
 
-        const updatedRequestData = await transactionRequest.requestData()
-        const updatedLogs = updatedRequestData.logs.find(e => e.event === 'RequestData')
-        // requestData.meta.isCancelled === true
-        assert(updatedLogs.args.bools[0] === true)
+        const requestDataRefresh = await parseRequestData(txRequest) 
+
+        expect(requestDataRefresh.meta.isCancelled)
+        .to.be.true
 
         await waitUntilBlock(0, windowStart)
 
-        const executeTx = await transactionRequest.execute({gas: 3000000})
-            .should.be.rejectedWith('VM Exception while processing transaction: revert')
-        /// TODO: Either change the smart contracts to revert the transaction or
-        ///       pull out the logs here and check the abort reason.
-        
-        const mostUpdatedRequestData = await transactionRequest.requestData()
-        const mostUpdatedLogs = mostUpdatedRequestData.logs.find(e => e.event === 'RequestData')
-        // requestData.meta.wasCalled === false
-        assert(mostUpdatedLogs.args.bools[1] === false)
+        const executeTx = await txRequest.execute({gas: 3000000})
 
-        const updatedWasCalled = await transactionRecorder.wasCalled()
-        assert(updatedWasCalled === false)
+        const requestDataRefreshTwo = await parseRequestData(txRequest)
+        
+        expect(await txRecorder.wasCalled())
+        .to.be.false
+
+        expect(requestDataRefreshTwo.meta.wasCalled)
+        .to.be.false
+
+        expect(wasAborted(executeTx))
+        .to.be.true 
+
+        expect(parseAbortData(executeTx).find(reason => reason === 'WasCancelled'))
+        .to.exist 
     })
 })
